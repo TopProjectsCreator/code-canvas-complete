ef1762e17273a00739612f05f37a2aa435c0a911
TopProjectsCreator <ishaanbans@gmail.com>
2026-05-12 12:22:14 -0700

    Delete lumina/codex directory

--- lumina/codex/client/src/store/useApp.js
+++
@@ -1,2 +0,0 @@
-import { create } from 'zustand';
-export const useApp = create((set)=>({ token:localStorage.getItem('token'), user:null, setAuth:(token,user)=>{localStorage.setItem('token',token);set({token,user});} }));
